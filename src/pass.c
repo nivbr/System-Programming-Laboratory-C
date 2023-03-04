b496e00822fa9d5d598c6c2acf1a4992dd55821c
Niv Briman <niv.briman@gmail.com>
2023-03-04 18:25:42 +0200

    Fixed tokening for .extern, also for .data when no LABEL before it

--- src/pass.c
+++ src/pass.c
@@ -1,18 +1,19 @@
 #include "symbolChart.h"
 #include <ctype.h>
 
-#define WORD_FORMAT " %80s"
 #define FIRST_MEM_CELL 100
 
 Line* newSymbol(char* symbol,int value, int baseAdrs, int offset, bool attribute[4]);
-bool pass1(char* filename ,symbolChart * chart, int dataMem[256][2], int codeMem[256][2]); /*return true if no errors !*/
-void doLine(char* cur_line,int* IC, int* DC, symbolChart * chart, int dataMem[256][2], int codeMem[256][2], bool * errorFlag); /*return true if OK, false if error found*/
+bool pass1(char* filename ,symbolChart * chart, int dataMem[256], int codeMem[MEMORY_SIZE]); /*return true if no errors !*/
+void doLine(char* cur_line,int* IC, int* DC, symbolChart * chart, int dataMem[MEMORY_SIZE], int codeMem[MEMORY_SIZE], bool * errorFlag); /*return true if OK, false if error found*/
 void clearString(char * s);
+bool stringIsEmpty(char* s);
+void printMemPic(int mem[MEMORY_SIZE], const char* headline);
 
 bool pass(char** filesList[4],int listCounters[4]){
     symbolChart * chart;
-    int dataMem[256][2]={0};
-    int codeMem[256][2]={0};
+    int dataMem[256]={0};
+    int codeMem[256]={0};
     int i;
     printf(">Parsing process\n\n");
     for (i=0;i<listCounters[afterMacro];i++){
@@ -21,6 +22,7 @@ bool pass(char** filesList[4],int listCounters[4]){
         if(pass1(filesList[afterMacro][i],chart,dataMem,codeMem)){  /*no error found*/
             printSymbolChart(chart);    /*debug print*/
             deleteSymbolChart(chart);
+            printMemPic(dataMem,"data");
             /*pass2(...)*/
         }
         printf(">End Parsing [%s]\n\n",filesList[afterMacro][i]);
@@ -29,7 +31,7 @@ bool pass(char** filesList[4],int listCounters[4]){
     return true;
 }
 
-bool pass1(char* filename ,symbolChart * chart, int dataMem[256][2], int codeMem[256][2]){
+bool pass1(char* filename ,symbolChart * chart, int dataMem[MEMORY_SIZE], int codeMem[MEMORY_SIZE]){
     char cur_line[LINE_LENGTH]= "";  /*holds current line*/
     int IC=0,DC=0;
     bool errorFlag= false;
@@ -47,55 +49,57 @@ bool pass1(char* filename ,symbolChart * chart, int dataMem[256][2], int codeMem
     return true;
 }
 
-void doLine(char* cur_line,int* IC, int* DC, symbolChart * chart, int dataMem[256][2], int codeMem[256][2], bool * errorFlag){
+void doLine(char* cur_line,int* IC, int* DC, symbolChart * chart, int dataMem[MEMORY_SIZE], int codeMem[MEMORY_SIZE], bool * errorFlag){
     Line * line = NULL;
     bool atr[4] = {false,false,false,false}, symbolFlag= false;   /*errorFlag is on if errors found- the pass2 won't happen*/
-    bool flagData= false, flagString=false;
     int L=0;
-    char token[LINE_LENGTH]= ""; /*holds current word*/
+    char* token = NULL;
     char symbol[LINE_LENGTH]="";    /*holds the symbol if there is*/
-    char tempLine[LINE_LENGTH]="";
-    sscanf(cur_line,WORD_FORMAT,token); /*get first token*/
-    strcpy(tempLine,cur_line);
+    token = strtok(cur_line," ");   /*get first token*/
     if(!strcmp(token,";")) /*ignore line that starts with ';' */
         return;
     /*if a symbol was anounced*/
     if(token[strlen(token)-1]==':' && !strcmp(token,":")){
         symbolFlag= true;
         strcpy(symbol,token);               /*put token into symbol*/
-        strcpy(token,strtok(tempLine,token));   /*update token to be the rest of the line*/
-        strtok(token," ");    /*put the seconde token as first*/
+        /*strcpy(token,strtok(NULL," "));*/   /*update token to be the rest of the line*/
+        token = strtok(NULL," ");
 
     }else{strcpy(symbol,"");}   /*reset symbol*/
     /*if there was a symbol definition: then symbol holds it and token holds the first op*/
-    /*else:  symbol holds nothing and token holds the first op*/
-    
+    /*else:  symbol holds nothing and token holds the first op*/    
+    clearString(token);
     if(!strcmp(token,".data") || !strcmp(token,".string")){ /*data type*/
-        flagData=false;
-        flagString=false;
-        if(!strcmp(token,".data"))  /*.data*/
-            flagData=true;
-        else                        /*.string*/
-            flagString=true;
-        
+        L=0;
+        if(!strcmp(token,".data")){  /*.data*/
+            printf("\n\tBEFORE: IN DATA-> token: [%s] ,line:[%s] \n\n",token,cur_line);        
+            while(!stringIsEmpty(token)){
+                printf("\n\tAdd: [%d][%d]\n",(*DC),atoi(token));
+                dataMem[*DC]= atoi(token);
+                *DC += 1;
+                L++;
+                token = strtok(NULL,",");
+            }
+        }
+        else{    /*.string*/
+            ;
+        }
         if (symbolFlag){
             /**/;
         }
         /*define data*/
         /*write data to mem*/
         /*update DC (by last stages)*/
-        
 
     }else if(!strcmp(token,".extern")){ /*external (OK to assume: no lables before)*/        
-        /*getNextToken(cur_line,token);*/
-        strcpy(token, strtok(tempLine, " "));
-        strcpy(token , strtok(NULL, ","));
+        token = strtok(NULL," ");
         clearString(token);
         /*insert to symbolChart */
         atr[external]=true; /*set the attributes to to external for it to be copied*/            
         line = newSymbol(token,0,0,0,atr);
         atr[external]=false;    /*was COPIED so can be reset*/
-        insertSymbol(line,chart);   /*insert to chart*/               
+        insertSymbol(line,chart);   /*insert to chart*/
+        memset(token,0,strlen(token));
     }else if(!strcmp(token,".entry")){  /*entry- will be done by pass2*/
         return;
     }else{  /*just code*/
@@ -117,7 +121,7 @@ void clearString(char * s){
             s[i]=0;
         else break;
     for(i=0;i<strlen(s);i++)
-        if(!isalnum(s[i]))
+        if(isspace(s[i]))
             t++;
         else break;
     strcpy(s,t);
@@ -137,4 +141,23 @@ Line* newSymbol(char* symbol,int value, int baseAdrs, int offset, bool attribute
     for(i=0;i<4;i++)
         line->attributes[i]=attribute[i];
     return line;
+}
+
+bool stringIsEmpty(char* s){
+    int i;
+    if(!s)
+        return true;
+    for(i=0;i<strlen(s);i++)
+        if(!isspace(s[i]))
+            return false;
+    return true;
+}
+
+void printMemPic(int mem[MEMORY_SIZE], const char* headline){
+    int i;
+    printf("\t--%s memory map--\n",headline);
+    for(i=0;i<MEMORY_SIZE;i++)
+        if(mem[i]!=0)
+            printf("\t\t%d | %d\n",i,mem[i]);
+    printf("\t----------------------\n\n");
 }
